18650b7b18a9246230bf2cf7128b431f66b20f2a
Orkhan Karimov <naxro.ok1@gmail.com>
2026-03-27 18:06:06 +0400

    chore: update dependencies in shared package to latest versions of honestjs and hono

--- shared/package/dependencies.js
+++ shared/package/dependencies.js
@@ -4,8 +4,8 @@
  */
 export default function getDependencies() {
 	return {
-		honestjs: '^0.1.14',
-		hono: '^4.12.8',
+		honestjs: '^0.1.18',
+		hono: '^4.12.9',
 		'reflect-metadata': '^0.2.2',
 		'http-essentials': '^1.4.0'
 	}
